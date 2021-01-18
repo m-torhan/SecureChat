93fd7e67c92850353aadc6b6767d480eaab777f2
maciek97x <maciek97x@o2.pl>
2021-01-18 08:38:32 +0100

    [dr] add double ratchet implementation and some tests

---
+++ double_ratchet.py
@@ -0,0 +1,209 @@
+import curve25519
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import HKDF
+from Crypto.Hash import HMAC, SHA256
+
+from debug_tools import *
+
+class DoubleRatchetState(object):
+    def __init__(self, dhs: bytes, dhr: bytes, rk: bytes, cks: bytes, ckr: bytes):
+        self.dhs = dhs
+        self.dhr = dhr
+        self.rk = rk
+        self.cks = cks
+        self.ckr = ckr
+        self.ns = 0
+        self.nr = 0
+        self.pn = 0
+        self.mkskipped = {}
+
+class DoubleRatchetHeader(object):
+    def __init__(self, dh: bytes, pn: int, n: int):
+        self.dh = dh
+        self.pn = pn
+        self.n =  n
+    
+    def to_bytes(self):
+        return bytes(self.dh) + bytes(self.pn) + bytes(self.n)
+
+class DoubleRatchetPacket(object):
+    def __init__(self, header:  DoubleRatchetHeader, payload: bytes):
+        self.__header = header
+        self.__payload = payload
+    
+    @property
+    def header(self):
+        return self.__header
+        
+    @property
+    def payload(self):
+        return self.__payload
+
+class DoubleRatchetKeyPair(object):
+    def __init__(self, public_key: bytes, private_key: bytes):
+        self.__public_key = public_key
+        self.__private_key = private_key
+    
+    @property
+    def public_key(self):
+        return self.__public_key
+        
+    @property
+    def private_key(self):
+        return self.__private_key
+    
+    def get_agreement(self, public_key: bytes):
+        return curve25519.calculateAgreement(public_key, self.__private_key)
+
+class DoubleRatchetKeyPairGenerator(object):
+    @staticmethod
+    def generate_key_pair():
+        r = get_random_bytes(32)
+        return DoubleRatchetKeyPair(curve25519.generatePublicKey(r), curve25519.generatePrivateKey(r))
+
+class DoubleRatchet(object):
+    MAX_SKIP = 10
+    app_specific_info = bytes(b'\x00')*32
+    mac_size = 16
+    fixed_nonce = b'\x24\x7b\x67\x10\x19\x75\x65\x41\x10\x2e'
+
+    @staticmethod
+    def create_initiator_state(sk: bytes, pk: bytes) -> DoubleRatchetState:
+        key_pair = DoubleRatchetKeyPairGenerator.generate_key_pair()
+        kdf_rk = DoubleRatchet.__kdf_rk(sk, DoubleRatchet.__dh(key_pair, pk))
+        return DoubleRatchetState(key_pair, pk, *kdf_rk, None)
+    
+    @staticmethod
+    def create_receiver_state(key_pair: DoubleRatchetKeyPair, sk: bytes) -> DoubleRatchetState:
+        return DoubleRatchetState(key_pair, None, sk, None, None)
+    
+    @staticmethod
+    def ratchet_encrypt(state: DoubleRatchetState, plaintext: str, ad: bytes) -> DoubleRatchetPacket:
+        kdf = DoubleRatchet.__kdf_ck(state.cks)
+        state.cks  = kdf[0]
+        mk = kdf[1]
+        header = DoubleRatchetHeader(state.dhs.public_key, state.pn, state.ns)
+        state.ns +=  1
+        packet = DoubleRatchetPacket(header, DoubleRatchet.__encrypt(mk, plaintext.encode(), ad + header.to_bytes()))
+        return packet
+    
+    @staticmethod
+    def ratchet_decrypt(state: DoubleRatchetState, packet: DoubleRatchetPacket, ad: bytes) -> bytes:
+        plaintext = DoubleRatchet.__try_skipped_message_keys(state, packet, ad)
+        if (plaintext != None):
+            return plaintext
+        elif state.dhr == None or packet.header.dh != state.dhr:
+            DoubleRatchet.__skip_message_keys(state, packet.header.pn)
+            DoubleRatchet.__dh_ratchet(state, packet.header)
+        
+        DoubleRatchet.__skip_message_keys(state, packet.header.n)
+        kdf = DoubleRatchet.__kdf_ck(state.ckr)
+        state.ckr = kdf[0]
+        mk = kdf[1]
+        state.nr += 1
+        return DoubleRatchet.__decrypt(mk, packet.payload, ad + packet.header.to_bytes())
+
+    @staticmethod
+    def __try_skipped_message_keys(state: DoubleRatchetState, packet:  DoubleRatchetPacket, ad: bytes) -> bytes:
+        if packet.header.dh not in state.mkskipped.keys():
+            return None
+        mk = state.mkskipped[packet.header.dh]
+        if packet.header.n not in mk.keys():
+            return None
+        mk = mk[packet.header.n]
+        del state.mkskipped[packet.header.dh][packet.header.n]
+        return DoubleRatchet.__decrypt(mk, packet.payload, ad + packet.header.to_bytes())
+
+    @staticmethod
+    def __skip_message_keys(state: DoubleRatchetState, until: int):
+        if state.nr + DoubleRatchet.MAX_SKIP < until:
+            raise Exception('MAX_SKIP exceeded')
+        
+        if state.ckr is not None:
+            while state.nr  < until:
+                ck_step = kdf_ck(state.ckr)
+
+                state.ckr = ck_step[0]
+                mk = ck_step[1]
+                state.nr += 1
+
+                if state.dhr not in state.mkskipped.keys():
+                    state.mkskipped[state.dhr] = {}
+                state.mkskipped[state.dhr][state.nr] = mk
+                state.nr +=  1
+    
+    @staticmethod
+    def __dh_ratchet(state: DoubleRatchetState, header: DoubleRatchetHeader):
+        state.pn = state.ns
+        state.ns = 0
+        state.nr = 0
+        state.dhr = header.dh
+        kdf = DoubleRatchet.__kdf_rk(state.rk, DoubleRatchet.__dh(state.dhs, state.dhr))
+        state.rk = kdf[0]
+        state.ckr = kdf[1]
+        state.dhs = DoubleRatchet.__generate_dh()
+        kdf = DoubleRatchet.__kdf_rk(state.rk, DoubleRatchet.__dh(state.dhs, state.dhr))
+        state.rk = kdf[0]
+        state.cks = kdf[1]
+
+    @staticmethod
+    def __generate_dh() -> DoubleRatchetKeyPair:
+        return DoubleRatchetKeyPairGenerator.generate_key_pair()
+    
+    @staticmethod
+    def __dh(dh_pair: DoubleRatchetKeyPair, dh_pub: bytes) -> bytes:
+        dh_pair.get_agreement(dh_pub)
+    
+    @staticmethod
+    def __encrypt(mk: bytes, plaintext: bytes, ad: bytes) -> bytes:
+        print_debug(mk, type(mk))
+        block_cipher = AES.new(key=mk, mode=AES.MODE_GCM, nonce=DoubleRatchet.fixed_nonce, mac_len=DoubleRatchet.mac_size)
+        return block_cipher.encrypt(plaintext)
+        
+    @staticmethod
+    def __decrypt(mk: bytes, ciphertext: bytes, ad: bytes) -> bytes:
+        block_cipher = AES.new(mk, AES.MODE_GCM, nonce=DoubleRatchet.fixed_nonce, mac_len=DoubleRatchet.mac_size)
+        return block_cipher.decrypt(ciphertext)
+
+    @staticmethod
+    def __kdf_rk(rk: bytes, dh_out: bytes) -> (bytes, bytes):
+        ret = HKDF(dh_out, key_len=32, salt=rk, hashmod=SHA256, num_keys=1)
+        return ret[:32], ret[32:]
+        
+    @staticmethod
+    def __kdf_ck(ck: bytes) -> (bytes, bytes):
+        hmac = HMAC.new(ck, b'\x00'*64, SHA256)
+        ret = hmac.hexdigest().encode()
+        return ret[:32], ret[32:]
+
+class User(object):
+    def  __init__(self, name: str, dr_state: DoubleRatchetState, dr: DoubleRatchet):
+        self.__name  = name
+        self.__dr_state = dr_state
+        self.__dr = dr
+        self.log = []
+    
+    @property
+    def name(self):
+        return self.__name
+
+    def send_message(self, other, message: str):
+        packet = self.__dr.ratchet_encrypt(self.__dr_state, message, other.name.encode())
+        self.__sendPacket(other, packet)
+
+    def force_skip_message(self, other, message: str):
+        return self.__dr.ratchet_encrypt(self.__dr_state, message, other.name.encode())
+
+    def deliver_skipped_message(self, other, packet: DoubleRatchetPacket):
+        self.__sendPacket(other, packet)
+    
+    def __on_message_received(self, other, message: str):
+        self.log.append(message)
+    
+    def __sendPacket(self, other, packet: DoubleRatchetPacket):
+        other.__on_packed_received(self, packet)
+    
+    def __on_packed_received(self, other, packet: DoubleRatchetPacket):
+        message = self.__dr.ratchet_decrypt(self.__dr_state, packet, self.name.encode()).decode()
+        self.__on_message_received(other, message)
--- unittests.py
+++ unittests.py
@@ -3,7 +3,7 @@ import unittest
 from time import sleep
 import socket
 from connection import *
-
+from double_ratchet import *
 
 class ConnectionTest(unittest.TestCase):
     def test_connect(self):
@@ -46,6 +46,35 @@ class ConnectionTest(unittest.TestCase):
         connection_1.close()
         connection_2.close()
         listener.close()
+
+class DoubleRatchetTest(unittest.TestCase):
+    def test_1(self):
+        dr = DoubleRatchet()
+
+        key_pair_alice = DoubleRatchetKeyPairGenerator.generate_key_pair()
+        key_pair_bob = DoubleRatchetKeyPairGenerator.generate_key_pair()
+
+        sk_bob = key_pair_bob.get_agreement(key_pair_alice.public_key)
+        sk_alice = key_pair_alice.get_agreement(key_pair_bob.public_key)
+
+        bob = User(
+            "Bob",
+            dr.create_receiver_state(key_pair_bob, sk_bob),
+            dr)
+        
+        alice = User(
+            "Alice",
+            dr.create_initiator_state(sk_alice, key_pair_bob.public_key),
+            dr)
+        
+        alice.send_message(bob, "test")
+        self.assertEqual(bob.log[0], "test")
+
+        alice.send_message(bob, "test2")
+        self.assertEqual(bob.log[1], "test2")
+
+        bob.send_message(alice, "test3")
+        self.assertEqual(alice.log[0], "test3")
 
 
 if __name__ == '__main__':
